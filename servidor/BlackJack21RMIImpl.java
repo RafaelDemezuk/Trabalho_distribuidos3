823de91e9257fec2b4e4c7a4f25c786579014bed
Rafael Boldrini Demezuk <rafaeldemezuk@hotmail.com>
2025-06-23 15:20:48 -0300

    BaralhoImp construtor

--- servidor/BlackJack21RMIImpl.java
+++ servidor/BlackJack21RMIImpl.java
@@ -3,14 +3,14 @@ package servidor;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-import interfaces.OlaMundoRMI;
+import interfaces.BlackJack21;
 
 // 3. A classe de DEVE implementar a interface remota
-public class OlaMundoRMIImpl implements OlaMundoRMI {
-
+public class BlackJack21RMIImpl implements BlackJack21 {
+    Baralho baralho;
     // 4. Implementação do método da interface remota
     @Override
-    public String dizerOla() throws RemoteException {
+    public Carta hit() throws RemoteException {
         System.out.println("[SERVIDOR] Método dizerOla() foi chamado!");
         return "Olá Mundo do RMI direto do Servidor!";
     }
